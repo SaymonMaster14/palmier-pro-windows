5f48a3e035ec7f6bfd6e668cc56ca9aa3dd277cc
SaymonMaster14 <saymon@example.com>
2026-09-13 17:20:34 -0300

    Remove leftover patch scripts

--- asm-fix2.cjs
+++
@@ -1,8 +0,0 @@
-﻿const fs = require("node:fs");
-const f = "apps/editor/src/main.js";
-let s = fs.readFileSync(f, "utf8");
-const oldT = "    expJobs.set(id, { ...job, ctrl });";
-if (!s.includes(oldT)) throw new Error("anchor?");
-s = s.replace(oldT, "    job.ctrl = ctrl;\n    expJobs.set(id, job);");
-fs.writeFileSync(f, s);
-console.log("fixed");
