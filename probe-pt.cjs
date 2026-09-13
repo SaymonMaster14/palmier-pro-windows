5513838ec346ffc51fb462699fe05600e62649f8
SaymonMaster14 <saymon@example.com>
2026-09-13 20:19:16 -0300

    Remove turn scaffolding scripts

--- probe-pt.cjs
+++
@@ -1,4 +0,0 @@
-﻿const fs = require("node:fs");
-const s = fs.readFileSync("apps/editor/renderer/index.html", "utf8");
-const i = s.indexOf("function paintTracks()");
-console.log(JSON.stringify(s.slice(i, i + 1100)));
